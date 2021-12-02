1fcdf871b5174eaf0e31d82bc9ed632a2362a9fa
Julien Gunther <jgunther021@gmail.com>
2021-12-02 16:18:35 +0100

    main example update

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from utils import SocialMedias, get_credentials, Scales
 
 reddit = RedditBot(get_credentials())
 data = reddit.save_images_from_subreddit(
-    amount=1,
-    subreddits=("FoodPorn",),
+    amount=5,
+    subreddits=("dankmemes",),
     scale=Scales.InstagramPhotoSquare
 )
 video_path = reddit.create_video(data)
