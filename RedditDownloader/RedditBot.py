5ea57bef4f747dec22328f3c6e80273cf9646649
Julien Gunther <jgunther021@gmail.com>
2021-12-02 09:43:33 +0100

    private methods and properties, scale image as class

--- RedditDownloader/RedditBot.py
+++ RedditDownloader/RedditBot.py
@@ -7,10 +7,10 @@ import praw
 import requests
 from environs import Env
 
-from .ScaleImages import scale_image
+from .ScaleImages import Scale
 
 
-class RedditBot:
+class RedditBot(Scale):
     def __init__(self, env: Env, save_path=os.getcwd(), log: bool = False) -> None:
         """Reddit downloader class
 
@@ -19,54 +19,57 @@ class RedditBot:
         :param save_path: A string or pathlike to a folder where images and data will be downloaded (cwd by default).
         """
 
-        self.log = log
+        # init Scale class
+        super(Scale).__init__()
+
+        self.__log = log
 
         # connect to reddit
-        self._reddit = praw.Reddit(
+        self.__reddit = praw.Reddit(
             client_id=env("REDDIT_CLIENT_ID"),
             client_secret=env("REDDIT_CLIENT_SECRET"),
             user_agent=env("REDDIT_USER_AGENT")
         )
 
         # define image format that we want to query
-        self._accepted_format = ["jpg", "png", "gif"]
+        self.__accepted_format = ["jpg", "png", "gif"]
 
         # define a path with folder that has today's date
-        self._today_data_path = os.path.join(save_path, f"data\\{date.today().strftime('%m%d%Y')}\\")
+        self.__today_data_path = os.path.join(save_path, f"data\\{date.today().strftime('%m%d%Y')}\\")
 
         # define path to utility file like already_downloaded.json
-        self._already_downloaded_path = os.path.join(save_path, f"data/utils/")
+        self.__already_downloaded_path = os.path.join(save_path, f"data/utils/")
 
         # define file name for already downloaded images
-        self._already_downloaded_json = "already_downloaded.json"
+        self.__already_downloaded_json = "already_downloaded.json"
 
         # create already_downloaded.json if not exists
-        if not os.path.isdir(self._already_downloaded_path):
-            os.makedirs(self._already_downloaded_path, exist_ok=True)
-            with open(file=f"{self._already_downloaded_path}{self._already_downloaded_json}", mode="w") as f:
+        if not os.path.isdir(self.__already_downloaded_path):
+            os.makedirs(self.__already_downloaded_path, exist_ok=True)
+            with open(file=f"{self.__already_downloaded_path}{self.__already_downloaded_json}", mode="w"):
                 pass
 
         # load json file to class
-        with open(file=f"{self._already_downloaded_path}{self._already_downloaded_json}", mode="r") as f:
+        with open(file=f"{self.__already_downloaded_path}{self.__already_downloaded_json}", mode="r") as f:
             try:
                 self._already_downloaded = json.load(f)
             except json.decoder.JSONDecodeError:
                 self._already_downloaded = []
 
-    def _create_subreddit_folder(self, subreddit: str) -> str:
+    def __create_subreddit_folder(self, subreddit: str) -> str:
 
-        sub_path = os.path.join(self._today_data_path, f"{subreddit}")
+        sub_path = os.path.join(self.__today_data_path, f"{subreddit}")
         if not os.path.isdir(sub_path):
             os.makedirs(sub_path, exist_ok=True)
             os.mkdir(os.path.join(sub_path, "images"))
             os.mkdir(os.path.join(sub_path, "data"))
         return sub_path
 
-    def _get_posts_from_subreddit(self, subreddit: str, over_18: bool, amount: int, accepted_format: Tuple[str]) -> \
+    def __get_posts_from_subreddit(self, subreddit: str, over_18: bool, amount: int, accepted_format: Tuple[str]) -> \
             List[praw.reddit.models.Submission]:
 
         submissions = []
-        for submission in self._reddit.subreddit(subreddit).top("day", limit=1000):
+        for submission in self.__reddit.subreddit(subreddit).top("day", limit=1000):
             if not submission.stickied and submission.url.lower()[-3:] in accepted_format and \
                     submission.over_18 == over_18 and submission.id not in self._already_downloaded:
                 submissions.append(submission)
@@ -74,18 +77,18 @@ class RedditBot:
                 break
         return submissions
 
-    @staticmethod
-    def _save_submission_image(save_path: str, submission: praw.reddit.models.Submission, scale: tuple,
-                               replace_resized: bool) -> None:
+    def __save_submission_image(self, save_path: str, submission: praw.reddit.models.Submission, scale: tuple,
+                                replace_resized: bool) -> None:
 
         img = requests.get(submission.url.lower())
         with open(save_path, "wb") as f:
             f.write(img.content)
 
         if scale:
-            scale_image(save_path, scale, replace_resized)
+            self._scale_image(save_path, scale, replace_resized)
 
-    def _save_submission_data(self, save_path: str, image_path: str, submission: praw.reddit.models.Submission) -> None:
+    def __save_submission_data(self, save_path: str, image_path: str,
+                               submission: praw.reddit.models.Submission) -> None:
         submission.comment_sort = "best"
         best_comment = None
         best_comment_2 = None
@@ -123,7 +126,7 @@ class RedditBot:
         }
 
         self._already_downloaded.append(submission.id)
-        with open(f"{self._already_downloaded_path}{self._already_downloaded_json}", mode="w",
+        with open(f"{self.__already_downloaded_path}{self.__already_downloaded_json}", mode="w",
                   encoding="utf-8-sig") as f:
             json.dump(self._already_downloaded, f)
         with open(f"{save_path}", mode="w", encoding="utf-8-sig") as f:
@@ -149,15 +152,19 @@ class RedditBot:
         """
 
         for subreddit in subreddits:
-            save_path = self._create_subreddit_folder(subreddit)
-            submissions = self._get_posts_from_subreddit(subreddit, nsfw, amount, filetypes)
+            save_path = self.__create_subreddit_folder(subreddit)
+            if self.__log:
+                print(f"Search for images on the {subreddit} subreddit...")
+            submissions = self.__get_posts_from_subreddit(subreddit, nsfw, amount, filetypes)
+            if self.__log:
+                print("Images found ! start downloading them...")
             for submission in submissions:
                 image_path = f"{save_path}\\images\\{submission.id}{submission.url.lower()[-4:]}"
-                self._save_submission_image(image_path, submission, scale, replace_resized)
-                self._save_submission_data(f"{save_path}\\data\\{submission.id}.json", image_path, submission)
-                if self.log:
+                self.__save_submission_image(image_path, submission, scale, replace_resized)
+                self.__save_submission_data(f"{save_path}\\data\\{submission.id}.json", image_path, submission)
+                if self.__log:
                     print(f"image downloaded from /r/{subreddit}.")
-            if self.log:
+            if self.__log:
                 print(f"{len(submissions)} images from /r/{subreddit} have been downloaded.")
-        if self.log:
+        if self.__log:
             print(f"Download finished for the following subreddit(s) : {', '.join(subreddits)}.")
